9adefc4df539a2de8f5d397c01359cf2e92a307e
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-10-12 10:30:03 +0100

    Added UI for auto end date for services

--- src/views/services/show/DetailsTab.vue
+++ src/views/services/show/DetailsTab.vue
@@ -70,6 +70,10 @@
           />
         </gov-table-row>
         <gov-table-row>
+          <gov-table-header top scope="row">End date</gov-table-header>
+          <gov-table-cell>{{ service.ends_at | endsAt }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
           <gov-table-header top scope="row"
             >Gallery items ({{ imageUrls.length }})</gov-table-header
           >
@@ -84,6 +88,7 @@
 </template>
 
 <script>
+import moment from "moment";
 import CkCarousel from "@/components/Ck/CkCarousel";
 
 export default {
@@ -101,6 +106,15 @@ export default {
   computed: {
     imageUrls() {
       return this.service.gallery_items.map(galleryItem => galleryItem.url);
+    }
+  },
+
+  filters: {
+    endsAt(date) {
+      if (date === null || date === "") {
+        return "No end date";
+      }
+      return moment(date).format("D/M/YYYY");
     }
   }
 };
